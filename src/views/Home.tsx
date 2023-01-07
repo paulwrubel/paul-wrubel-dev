61a46b0e9a215273d5bc627eeac93daf0de866db
Paul Wrubel <pwrubel7@gmail.com>
2023-01-07 13:50:44 -0600

    add typing effect to homepage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
+
 import { Container, Paper, Typography } from "@mui/material";
 
+const commandSteps: { s: string; i: number; linear?: boolean }[] = [
+    { s: "p", i: 50 },
+    { s: "a", i: 50 },
+    { s: "u", i: 50 },
+    { s: "l", i: 50 },
+    { s: "w", i: 270 },
+    { s: "r", i: 70 },
+    { s: "u", i: 70 },
+    { s: "b", i: 70 },
+    { s: "e", i: 70 },
+    { s: "l", i: 70 },
+    { s: ".", i: 300 },
+    { s: "d", i: 300 },
+    { s: "e", i: 100 },
+    { s: "v", i: 100 },
+];
+
 const Home = () => {
+    const [commandIndex, setCommandIndex] = useState(0);
+    const [commandString, setCommandString] = useState<string>("> ");
+
+    const [showCaret, setShowCaret] = useState(true);
+
+    useEffect(() => {
+        if (commandIndex < commandSteps.length) {
+            const nextStep = commandSteps[commandIndex];
+            setTimeout(() => {
+                setCommandString(commandString + nextStep.s);
+            }, nextStep.i);
+            setCommandIndex(commandIndex + 1);
+        }
+    }, [commandString]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShowCaret(!showCaret);
+        }, 500);
+    }, [showCaret]);
+
     return (
         <Container sx={{ my: 10 }}>
             <Paper
@@ -16,7 +56,7 @@ const Home = () => {
                     fontSize="4rem"
                     fontFamily='"Source Code Pro", monospace'
                 >
-                    {"> ./paulwrubel.dev"}
+                    {commandString + (showCaret ? "_" : "")}
                 </Typography>
             </Paper>
             <Paper variant="outlined" elevation={0} sx={{ p: 2, m: 2 }}>
