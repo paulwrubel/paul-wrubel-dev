9828063f84c517333f418570b92b1b57dfc13f75
Paul Wrubel <pwrubel7@gmail.com>
2023-01-20 22:26:02 -0600

    remove more comments

--- src/tools/peerpong/index.tsx
+++ src/tools/peerpong/index.tsx
@@ -1,14 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// import { Box, Button, TextField, Typography } from "@mui/material";
-
-// import { DataConnection, Peer } from "peerjs";
-
-// import { randomCherryPickFromArray } from "utils";
-
-// import PeerPongSketch from "./PeerPongSketch";
-// import { ConnectionInfo, ConnectionState } from "./types";
-
 import { PeerJSWrapper } from "components/PeerJSWrapper";
 
 import PeerPongSketch from "./PeerPongSketch";
